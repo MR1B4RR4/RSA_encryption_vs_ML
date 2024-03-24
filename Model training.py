fe4b5372caf5d345b30e7833a19c200d5f76a054
MR1B4RR4 <sebastianidelc@gmail.com>
2024-03-24 14:44:22 -0600

    Model training

--- Model training.py
+++ Model training.py
@@ -1,39 +1,29 @@
 import pandas as pd	
 import numpy as np
 from sklearn.model_selection import train_test_split
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import Sequential
-from keras.layers import LSTM, Dense
-from keras.optimizers import Adam
+from autogluon.tabular import TabularPredictor
+
 from Crypto_algorithm import *
 
 
 df = pd.read_excel('DF_for_model_training.xlsx')
 df = df.drop('Unnamed: 0',axis=1)
 
-print(df)
+
+X = df.iloc[:,1:]
 
 
-# X_reshaped = np.array(df['encrypted_padded'].tolist()).reshape((len(df), max_sequence_length, 1))
-
-# y = np.array(df['token']).astype(float)
+y = df['token']
 
 
-# # Divide the Dataframe into 2 parts (train, test)
+#Divide the Dataframe into 2 parts (train, test)
 
-# X_train, X_test, y_train, y_test = train_test_split(X_reshaped, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# # Creating the model
+# Creating the model
 
-# input_dim = X_train.shape[1] 
-# output_dim = 1  
+predictor = TabularPredictor(label='token', path='autogluon_models').fit(train_data=pd.concat([X_train, y_train], axis=1))
 
-# model = Sequential()
-# model.add(LSTM(units=50, activation='relu', input_shape=(input_dim, 1)))
-# model.add(Dense(output_dim))
-# model.compile(optimizer=Adam(learning_rate=0.001, clipvalue=0.5), loss='mean_squared_error')
+performance = predictor.evaluate(pd.concat([X_test, y_test], axis=1))
 
-# # Training the model
-# model.fit(X_train, y_train, epochs=100, batch_size=32, validation_data=(X_test, y_test))
-
-# #model.save('model_keras.h5') 
+print(performance)
